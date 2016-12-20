4c63a5f8124ae05b0ffe95d634fd6723f7e620cf
122088387 <122088387@qq.com>
2016-12-20 18:45:48 +0800

    18:45

--- app/src/main/java/com/chaungying/address/adapter/DepartmentAdapter.java
+++ app/src/main/java/com/chaungying/address/adapter/DepartmentAdapter.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author 王晓赛 or 2016/7/29 ..
+ * @author 王晓赛 or 2016/7/29 ....
  */
 public class DepartmentAdapter extends BaseAdapter {
 
